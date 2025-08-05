c006e319300e6c85587d2d68aad8641a17b3ffad
RaviChatta <92024309+RaviChatta@users.noreply.github.com>
2025-08-05 22:50:32 +0530

    Update premium.py

--- premium.py
+++ premium.py
@@ -14,7 +14,7 @@ from pyrogram.types import (
 from pyrogram.enums import ParseMode
 from dotenv import load_dotenv
 from settings import config
-from ravi import *
+from rk import *
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
