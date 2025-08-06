16f59d13c40b641c698d58c86b960d63b45c5827
RaviChatta <92024309+RaviChatta@users.noreply.github.com>
2025-08-06 16:07:45 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -21,7 +21,6 @@ class Bot(Client):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            workers=20,
             sleep_threshold=15,
         )
         self.start_time = time.time()
